95ffa08b51d5341c8d707795b5e4d7fa14b73992
AndreJSON <andre.v.json@gmail.com>
2017-12-06 23:06:06 +0100

    Add simple mongoose connection.

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,33 @@ var express = require('express');
 var app = express();
 var chalk = require('chalk');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var ColSchema = new Schema({
+	name: String,
+	age: Number
+});
+
+var colModel = mongoose.model('col', ColSchema);
+
+colModel.find({}, function (err, docs) {
+	console.log(docs);
+});
+
+/*var LogEntrySchema = new Schema({
+
+});
+
+var UserSchema = new Schema({
+	name: {type: String, default: "Anonymous"},
+	points: {type: Number, default: 0},
+	position: {type: String},
+	log: [LogEntrySchema]
+});*/
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
